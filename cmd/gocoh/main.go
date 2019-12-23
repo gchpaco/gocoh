a5d65047069015dfa30040e2e8c57d9fa5fc8a92
Graham Hughes <graham.hughes@skipthedishes.ca>
2019-12-23 17:51:04 -0600

    Now verify files against the filesystem.

--- cmd/gocoh/main.go
+++ cmd/gocoh/main.go
@@ -1,24 +1,39 @@
 package main
 
 import (
-	"bufio"
+	"flag"
 	"fmt"
 	"github.com/gchpaco/gocoh/pkg/manifest"
 	"log"
-	"os"
+	"net/http"
 )
 
+var url = flag.String("manifest", "http://patch.savecoh.com/manifest.xml", "Manifest location to use")
+var dir = flag.String("base", ".", "Base directory to check for updates against")
+
 func main() {
-	manifest, err := manifest.Read(bufio.NewReader(os.Stdin))
+	flag.Parse()
+	resp, err := http.Get(*url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+	manifest, err := manifest.Read(resp.Body)
 	if err != nil {
 		log.Fatal(err)
 	}
 	fmt.Printf("Label: %s\n", manifest.Label)
-	for _, profile := range manifest.Profiles {
-		fmt.Printf("Profile %s:\n\tExec: %s\n\tOrder: %d\n\tParams: %s\n",
-			profile.Name, profile.Exec, profile.Order, profile.Params)
-	}
+	//for _, profile := range manifest.Profiles {
+	//	fmt.Printf("Profile %s:\n\tExec: %s\n\tOrder: %d\n\tParams: %s\n",
+	//		profile.Name, profile.Exec, profile.Order, profile.Params)
+	//}
 	for _, file := range manifest.Files {
-		fmt.Printf("File %s (%d sources)\n", file.Name, len(file.URLs))
+		fileOK, err := file.Verify(*dir)
+		if err != nil {
+			log.Fatal(err)
+		}
+		if !fileOK {
+			fmt.Printf("Need to redownload %s\n", file.Name)
+		}
 	}
 }
--- pkg/manifest/io.go
+++ pkg/manifest/io.go
@@ -1,8 +1,12 @@
 package manifest
 
 import (
+	"crypto/md5"
 	"encoding/xml"
+	"fmt"
 	"io"
+	"os"
+	"path/filepath"
 )
 
 // Read loads an XML wire document describing a CoH manifest and
@@ -11,4 +15,48 @@ func Read(r io.Reader) (manifest *Manifest, err error) {
 	manifest = new(Manifest)
 	err = xml.NewDecoder(r).Decode(&manifest)
 	return
+}
+
+// Verify checks to see if a file on disk matches the indicated File
+// in the manifest.  If false, the file should be redownloaded.
+func (file *File) Verify(base string) (bool, error) {
+	cleaned := filepath.Clean(file.Name)
+	if filepath.Base(cleaned) == ".." ||
+		filepath.Base(cleaned) == "." ||
+		filepath.Base(cleaned) == "/" {
+		return false, fmt.Errorf("Something weird going on with manifest; saw request for %s which is unsafe", cleaned)
+	}
+	localized := filepath.Join(base, cleaned)
+	if stat, err := os.Lstat(localized); err != nil {
+		if os.IsNotExist(err) {
+			return false, nil
+		} else {
+			return false, err
+		}
+	} else if !stat.Mode().IsRegular() {
+		return false, fmt.Errorf("Tried to read a file with a weird mode in %s, aborting", localized)
+	} else if stat.Size() != int64(file.Size) {
+		return false, nil
+	} else if hash, err := md5file(localized); err != nil {
+		return false, err
+	} else if fmt.Sprintf("%x", hash) != file.MD5 {
+		return false, nil
+	} else {
+		return true, nil
+	}
+}
+
+func md5file(file string) ([]byte, error) {
+	f, err := os.Open(file)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+
+	h := md5.New()
+	if _, err := io.Copy(h, f); err != nil {
+		return nil, err
+	}
+
+	return h.Sum(nil), nil
 }
